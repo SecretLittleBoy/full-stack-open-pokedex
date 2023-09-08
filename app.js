65ab4e3ccc0b0883eb09a7ce9301a08f37bdc8fc
LYH <3210101621@zju.edu.cn>
2023-09-08 11:11:20 +0800

    add fly.toml,try to deploy through github

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
 
